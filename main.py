fa50f704c7ed8d82b18c00e0cfb2d221df4572a8
hhhe11 <2518006788@qq.com>
2025-01-07 16:32:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -228,5 +228,5 @@ if __name__ == "__main__":
         note_ch = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch)
     os.system("pause")
